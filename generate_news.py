f98fce355c96a0df355ed700c80830cd16534f93
bwhurlburt-svg <bwhurlburt@gmail.com>
2026-01-04 00:36:57 -0400

    Update generate_news.py with intro and feed handling

--- generate_news.py
+++ generate_news.py
@@ -6,27 +6,21 @@ import os
 # --- CONFIG ---
 RSS_FEED = 'https://countryairradio.com/category/newsrss/feed/'  # Your actual RSS feed
 OUTPUT_FOLDER = './auto_news/'  # Folder for generated MP3s
-TOP_N_HEADLINES = 5  # Number of headlines to include
-INTRO_TEXT = "Here at CountryAirRadio.com we don't create the news, we promote the headlines from those who do! Letting you choose to further investigate those you find most interesting!"
+TOP_N_HEADLINES = 5
+INTRO_TEXT = "Here at CountryAirRadio.com we don’t create the news, we promote the headlines from those who do! Letting you choose to further investigate those you find most interesting!"
 
 # Ensure output folder exists
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
 
-# MP3 file name with timestamp
 MP3_FILENAME = f"CAR_LocalNews_{datetime.now().strftime('%Y%m%d_%H%M')}.mp3"
 
 # --- PARSE RSS ---
 feed = feedparser.parse(RSS_FEED)
-
-# Get top headlines
 headlines = [entry.title for entry in feed.entries[:TOP_N_HEADLINES]]
-
-# Combine intro with headlines
+news_text = INTRO_TEXT
 if headlines:
-    news_text = INTRO_TEXT + " Here are the top headlines: " + " ... ".join(headlines)
-else:
-    news_text = INTRO_TEXT + " There are currently no new headlines available."
+    news_text += " Here are the top headlines: " + " ... ".join(headlines)
 
 # --- GENERATE MP3 ---
 tts = gTTS(news_text)
